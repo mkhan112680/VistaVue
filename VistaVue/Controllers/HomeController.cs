95442a705c25c19a26698d610ae14fa182f2b2e1
Maqsood Khan <himan_sa@yahoo.com>
2018-06-25 12:45:59 -0400

    Trying

--- VistaVue/Controllers/HomeController.cs
+++ VistaVue/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace VistaVue.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index() 
         {
             return View();
         }
