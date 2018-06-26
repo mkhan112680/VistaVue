b269ea8942b7a521b00eb1d2d87abc8612849292
Maqsood Khan <himan_sa@yahoo.com>
2018-06-26 12:47:40 -0400

    Setup the layout page from vista source app

---
+++ VistaVue/Code/Helper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VistaVue.Code
+{
+    public class Helper
+    {
+        public static string GetLoggedInUser()
+        {
+            return "mkhan@hotmail.com";
+        }
+    }
+}
